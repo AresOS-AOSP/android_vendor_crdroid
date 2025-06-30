881c2c566a49eae66b0bca084ff45947de521d4f
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-01 05:14:46 +0800

    common.mk: add blur support
    [ChrisCatto]: Remove the way crdroid does it completely.
    
    Change-Id: Ie503986ef2642dbd474bfa5bee8d67688788d544

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -24,10 +24,13 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.input.video_enabled=false
 
 # Blurs
+TARGET_ENABLE_BLUR ?= true
 ifeq ($(TARGET_ENABLE_BLUR), true)
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    ro.sf.blurs_are_expensive=1 \
-    ro.surface_flinger.supports_background_blur=1
+    ro.custom.blur.enable=true
+else
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=false
 endif
 
 PRODUCT_PRODUCT_PROPERTIES += \
