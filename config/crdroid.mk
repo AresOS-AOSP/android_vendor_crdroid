f4d8fc2fa4d258b5d5f03fc69fac6133f2897607
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-16 16:11:25 +0000

    vendor: Update add blur support
    Change-Id: I1ced2b3c13c723886aaf46764ba294c18d6ff376
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -22,9 +22,19 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.input.video_enabled=false
 
-# Blur
-ifneq ($(TARGET_SUPPORTS_BLUR),false)
-PRODUCT_PRODUCT_PROPERTIES += ro.surface_flinger.supports_background_blur=1
+# Enable blur
+TARGET_ENABLE_BLUR ?= true
+ifeq ($(TARGET_ENABLE_BLUR),true)
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=true \
+    persist.sysui.disableBlur=false \
+    ro.surface_flinger.supports_background_blur=1
+else
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=false \
+    persist.sysui.disableBlur=true \
+    ro.surface_flinger.supports_background_blur=0
+endif
 
 # Cloned app exemption
 PRODUCT_COPY_FILES += \
