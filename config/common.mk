bd663a943444c365f2da90a2d8924d0728eb9781
ChrisCatto <chriscatto@proton.me>
2025-08-03 12:12:11 +0000

    vendor: Bringup AresOS
    Change-Id: I5a088013459e9cbef7453f861d2633f746bf140d
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- config/common.mk
+++ config/common.mk
@@ -7,7 +7,7 @@ $(call inherit-product-if-exists, vendor/certification/config.mk)
 # Allow vendor prebuilt repos to exclude themselves from bp scanning
 -include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
 
-PRODUCT_BRAND ?= crDroidAndroid
+PRODUCT_BRAND ?= AresOS
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -15,6 +15,12 @@ PRODUCT_PRODUCT_PROPERTIES += \
 else
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+endif
+
+# Gapps
+WITH_GMS ?= true
+ifeq ($(WITH_GMS),true)
+$(call inherit-product, vendor/pixel/gms/products/gms.mk)
 endif
 
 ifeq ($(PRODUCT_IS_ATV),true)
@@ -133,16 +139,13 @@ endif
 # Bootanimation
 TARGET_SCREEN_WIDTH ?= 1080
 TARGET_SCREEN_HEIGHT ?= 1920
-PRODUCT_PACKAGES += \
-    bootanimation.zip \
-    bootanimation-dark.zip
 
 # Lineage interfaces
 PRODUCT_PACKAGES += \
     framework_compatibility_matrix.lineage.xml
 
 # Lineage packages
-ifeq ($(PRODUCT_IS_ATV),)
+ifneq ($(WITH_GMS),true)
 PRODUCT_PACKAGES += \
     ExactCalculator \
     Jelly
@@ -263,9 +266,11 @@ endif
 $(call inherit-product, vendor/lineage/audio/audio.mk)
 
 # SetupWizard
+ifneq ($(WITH_GMS),true)
 PRODUCT_PRODUCT_PROPERTIES += \
     setupwizard.theme=glif_v4 \
     setupwizard.feature.day_night_mode_enabled=true
+endif
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/no-rro
 PRODUCT_PACKAGE_OVERLAYS += \
